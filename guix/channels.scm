9978bf97ad4ce879f7c1485ebd083347acb8c734
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-05-03 00:26:05 +0300

    renamed:    go/telemetry/local/go@go1.24.13-go1.24.13-linux-amd64-2026-04-04.v1.count -> go/telemetry/local/go@go1.26.1-go1.26.1-linux-amd64-2026-05-02.v1.count 	new file:   go/telemetry/local/local.2026-04-07.json 	new file:   guix/Advantages.org 	new file:   guix/Agents.org 	new file:   guix/Blueprint.org 	new file:   guix/Context.org 	new file:   guix/Journal.org 	new file:   guix/Laws.org 	new file:   guix/Philosophy.org 	new file:   guix/Principles.org 	new file:   guix/Quotes.org 	new file:   guix/Rules.org 	new file:   guix/State.org 	new file:   guix/TODO.org 	new file:   guix/TrapsExternal.org 	new file:   guix/TrapsInternal.org 	typechange: guix/channels.scm

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,17 +1,1 @@
-(list
-  (channel
-    (name 'guix)
-    (url "https://git.guix.gnu.org/guix.git")
-    (branch "master"))
-  (channel
-    (name 'manifold)
-    (url "file:///ManifoldOS")
-    (branch "master"))
-  (channel
-    (name 'nonguix)
-    (url "https://gitlab.com/nonguix/nonguix")
-    (introduction
-      (make-channel-introduction
-        "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-        (openpgp-fingerprint
-          "2A39 3FFF 68F4 EF7A 3D29 12AF 6F51 20A0 22FB B2D5")))))
+/gnu/store/08ihvmbr86qznrm7mdfcxjszcnkidx7v-channels.scm
