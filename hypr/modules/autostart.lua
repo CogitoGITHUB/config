bfdc48ae3cb41711b4ef356a047750cca3466101
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-08-09 20:31:18 +0300

    update

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -1,7 +1,7 @@
 -- modules/autostart.lua
 hl.on("hyprland.start", function()
     hl.exec_cmd("awww-daemon")
-    hl.exec_cmd("awww img /home/aoeu/.config/wallpapers/pics/roses/roses.jpg")
+    hl.exec_cmd("awww img /home/aoeu/.config/wallpaper/roses/rose-on-book.jpg")
     hl.exec_cmd("wezterm")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("omarchy-tui-shell start")
