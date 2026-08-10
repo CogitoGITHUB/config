2e8d367627840de9699d8ac25430e17b2e1f3f56
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-08-10 19:36:39 +0300

    update

---
+++ hypr/modules/easymotion.lua
@@ -0,0 +1,21 @@
+hl.config({
+    plugin = {
+        easymotion = {
+            textsize          = 15,
+            textcolor         = "rgba(ffffffff)",
+            bgcolor           = "rgba(000000ff)",
+            blur              = 0,
+            blurA             = 1.0,
+            xray              = 0,
+            textfont          = "Sans",
+            textpadding       = "0",
+            bordersize        = 0,
+            bordercolor       = "rgba(ffffffff)",
+            rounding          = 0,
+            fullscreen_action = "none",
+            motionkeys        = "aoeuidhtns",
+            motionlabels      = "AOEUIDHTNS",
+            only_special      = true,
+        },
+    },
+})
